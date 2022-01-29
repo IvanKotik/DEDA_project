dcd4f126f910e942e105d5ff9a56e6376b58cac1
IvanKotik <kotik.iv@outlook.com>
2022-01-29 16:49:40 +0100

    Finished cleaning descriptions and pick variables

--- Real estate data.R
+++ Real estate data.R
@@ -1,5 +1,6 @@
 library(tidyverse)
 
+# Data taken from: https://www.kaggle.com/corrieaar/apartment-rental-offers-in-germany/version/6
 # Importing data
 data <-   read_csv("C://Users//ivkot//Downloads//archive//immo_data.csv")
 
@@ -39,4 +40,46 @@ data_berlin <- relocate(data_berlin, totalRent, baseRent, serviceCharge, heating
 data_berlin <- relocate(data_berlin, livingSpace, noRooms, hasKitchen, balcony, facilities, description, typeOfFlat, garden, interiorQual, condition, cellar,  .after = heatingType)
 data_berlin <- relocate(data_berlin, lift,  .after = numberOfFloors)
 data_berlin <- relocate(data_berlin, yearConstructed,  .after = cellar)
+
+# Checking the "facilities" column for more useful characteristics
+# Guest WC's
+str_extract(data_berlin$facilities, "([zZ]wei.{0,4}B.d)|(ste.WC)")
+sort(str_extract(data_berlin$facilities, "([zZ]wei.{0,4}B.d)|(ste.WC)"))  # all good
+data_berlin$extrawc <- as.integer(sub("([zZ]wei.{0,4}B.d)|(ste.WC)", 1, (str_extract(data_berlin$facilities, "([zZ]wei.{0,4}B.d)|(ste.WC)"))))
+data_berlin <- relocate(data_berlin, extrawc, .after = hasKitchen)
+
+# dishwashing machine
+sort(str_extract(data_berlin$facilities, "Geschirrsp.lmaschine"))
+data_berlin$geschirrsp <- as.integer(sub("Geschirrsp.lmaschine", 1, (str_extract(data_berlin$facilities, "Geschirrsp.lmaschine"))))
+
+# washing machine
+sort(str_extract(data_berlin$facilities, "Waschmaschine"))
+data_berlin$washingm <- as.integer(sub("Waschmaschine", 1, (str_extract(data_berlin$facilities, "Waschmaschine"))))
+
+# sorting data
+data_berlin <- relocate(data_berlin, geschirrsp, washingm, .after = balcony)
+data_berlin <- relocate(data_berlin, description, facilities, .after = date)
+
+# checking the true-false columns for compatability
+sum(data_berlin$hasKitchen)
+sum(data_berlin$balcony)
+sum(data_berlin$garden)
+sum(data_berlin$cellar)
+sum(data_berlin$newlyConst)
+sum(data_berlin$lift)  # all good
+
+# making the NA-1 to true-false
+data_berlin$extrawc <- as.logical(replace_na(data_berlin$extrawc, 0))
+data_berlin$geschirrsp <- as.logical(replace_na(data_berlin$geschirrsp, 0))
+data_berlin$washingm <- as.logical(replace_na(data_berlin$washingm, 0))
+
+# Checking whether heatingTypes have a influence on the price
+ggplot(data_berlin[data_berlin[, "totalRent"]<5000, ], aes(x = livingSpace, y = totalRent, color = heatingType))+
+  geom_point()
+ggsave("heatingtypewithoutlines.png", dpi = 320, scale = 1)
+
+ggplot(data_berlin[data_berlin[, "totalRent"]<5000, ], aes(x = livingSpace, y = totalRent, color = heatingType))+
+  geom_point(size = 0.75, alpha = 0.2)+
+  geom_smooth(se = FALSE, method = lm, size = 0.7)  # based on the graps it can be seen that there is no heavy difference between the types
+ggsave("heatingtypewithlines.png", dpi = 320, scale = 1)
 
